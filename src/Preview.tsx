1e1980219e8297eaf91d6298a3ecbf56c0bf7ba5
Grant Custer <grantcuster@gmail.com>
2024-11-04 10:49:25 -0500

    add img caption to preview

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -2,13 +2,27 @@ import ReactMarkdown from "react-markdown";
 import { useAtom } from "jotai";
 import { activeTextAtom } from "./atoms";
 
+function MarkdownImageWithCaption({ alt, src }: { alt: string; src: string }) {
+  return (
+    <figure>
+      <img src={src} alt={alt} />
+      {alt && <figcaption>{alt}</figcaption>}
+    </figure>
+  );
+}
+
 export function Preview() {
   const [text] = useAtom(activeTextAtom);
 
   return (
-    <ReactMarkdown className="h-full w-full px-3 py-2 markdown overflow-auto sans-serif">
+    <ReactMarkdown
+      className="h-full w-full px-3 py-2 markdown overflow-auto sans-serif"
+      components={{
+        img: ({ node, ...props }) => <MarkdownImageWithCaption {...props} />,
+        p: ({ node, ...props }) => <div {...props} className="my-4" />,
+      }}
+   >
       {text}
     </ReactMarkdown>
   );
 }
-
